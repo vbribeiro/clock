c3f4172208ce1fefd2233ba07071e09a9be9a4c9
Beatriz Vieira <rvbeatriz73@gmail.com>
2022-07-10 22:10:06 -0300

    formatando números de unidades com 0 na frente

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ function relogio () {
     let minutos = data.getMinutes();
     let segundos = data.getSeconds();
 
+    if (hora < 10) {
+        hora = "0" + hora;
+    } else if (minutos < 10) {
+        minutos = "0" + minutos;
+    } else if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+
     let total = hora + ":" + minutos + ":" + segundos;
     
     document.getElementById('card--show').value = total;
